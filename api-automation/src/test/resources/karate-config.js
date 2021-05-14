88db3b8fbddbb0f68323adcb01815cf1cf7cda01
humahmed1 <73528337+humahmed1@users.noreply.github.com>
2021-05-14 20:22:31 +0100

    update cluster ip

--- api-automation/src/test/resources/karate-config.js
+++ api-automation/src/test/resources/karate-config.js
@@ -1,4 +1,4 @@
 function() {
     karate.configure('report', { showLog: true, showAllSteps: false } );
-    return esUrl='http://cmp-api-poc.com/es/customer';
+    return esUrl='http://cmp-poc-api.com:8070/es/customer';
 }
